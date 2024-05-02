31992b97f7fc31e2e88526c3813cb4dca86107bd
eliasgiannantonio <giannantonioelias@gmail.com>
2024-05-02 19:59:20 -0300

    Se agrega la funcionalidad de Modo Oscuro

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  ngOnInit() {
+    const themeToggle = document.getElementById('checkbox') as HTMLInputElement;
+    themeToggle.addEventListener('change', () => this.toggleTheme());
+  }
+
+  toggleTheme() {
+    console.log('Toggle theme function called');
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute('data-theme');
+    const newTheme = currentTheme === 'oscuro' ? 'claro' : 'oscuro';
+    root.setAttribute('data-theme', newTheme);
+  
+    // Cambia el estado del checkbox en función del tema
+    const themeToggle = document.getElementById('themeToggle') as HTMLInputElement;
+    themeToggle.checked = newTheme === 'oscuro';
+  }
 
 }
