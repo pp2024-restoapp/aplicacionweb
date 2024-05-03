1f50f7b58f741d00f404a9f29b7749db3b9b7384
maujoaquin <mauriciojarce1984@google.com>
2024-05-03 17:44:11 -0300

    se agregan ventanas modales para los detalles de los shows

--- frontend/src/app/pages/shows/shows.component.ts
+++ frontend/src/app/pages/shows/shows.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ShowsComponent {
 
+ 
+  popupVisible: boolean = false;
+  popupTitle: string = '';
+  popupDate: string = '';
+  popupDescription: string = '';
+  popupImageSrc: string = '';
+
+  openPopup(title: string, date: string, description: string,  imageSrc: string): void {
+    this.popupVisible = true;
+    this.popupTitle = title;
+    this.popupDate = date;
+    this.popupDescription = description;
+    this.popupImageSrc =imageSrc ;
+  }
+
+  closePopup(): void {
+    this.popupVisible = false;
+  }
 }
